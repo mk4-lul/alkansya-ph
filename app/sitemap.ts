46dcc10322222d43725cfbab2890f9fc34863ab6
mk4-lul <mk4lul@protonmail.com>
2026-04-02 12:24:40 +0800

    Update base URL in sitemap function

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const base = "https://alkansya.ph";
+  const base = "https://sentral.ph";
 
   return [
     { url: base, lastModified: new Date(), changeFrequency: "weekly", priority: 1 },
