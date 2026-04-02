844c4148fecf56575a451ae03075ffb578df7faf
mk4-lul <mk4lul@protonmail.com>
2026-04-02 12:55:46 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-const oldEnglish = localFont({ src: "../public/fonts/OPTIEngraversOldEnglish.otf", variable: "--font-old-english" });
+const oldEnglish = localFont({ src: "../public/fonts/EngraversOldEnglishBold.otf", variable: "--font-old-english" });
 const GA_ID = "G-NTCRT0NSFX";
 
 export const metadata: Metadata = {
